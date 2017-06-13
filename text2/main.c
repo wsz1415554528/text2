62d7194c7517faac92f8235f81f5c710075f0a23
wsz1415554528 <w20161104602@admindeiMac-007.local>
2017-06-13 09:31:15 +0800

    New

--- text2/main.c
+++ text2/main.c
@@ -6,10 +6,32 @@
 //  Copyright © 2017年 w20161104602. All rights reserved.
 //
 
-#include <stdio.h>
+#include<stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+int main(int argc, const char * argv[])
+{
+    
+    FILE *fr,*fw;
+    fr=fopen("//Users//w20161104602//Desktop//test//input.txt","r+");
+    fw=fopen("//Users//w20161104602//Desktop//test//output.txt","w");
+    int a[10];
+    int q,w,e;
+    for(q=0; q<10; q++)
+        fscanf(fr,"%d",&a[q]);
+    for(w=0;w<9; w++)
+        for(q=0; q<9-w; q++)
+        {
+            if(a[q]>a[q+1])
+            {
+                e=a[q];
+                a[q]=a[q+1];
+                a[q+1]=e;
+            }
+        }
+    for(q=0;q<10;q++)
+        fprintf(fw,"%d ",a[q]);
+    printf("\n");
     return 0;
+    
+    
 }
