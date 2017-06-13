ab47ecfca624fd5f96fffd04e58579b402450c1c
wsz1415554528 <w20161104602@admindeimac-007.local>
2017-06-13 09:30:45 +0800

    Initial Commit

---
+++ text2/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  text2
+//
+//  Created by w20161104602 on 2017/6/13.
+//  Copyright © 2017年 w20161104602. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
