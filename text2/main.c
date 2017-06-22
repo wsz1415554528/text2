e85f791ce44597a33ef62f01d7da77ae931078bb
wsz1415554528 <w20161104602@admindeiMac-007.local>
2017-06-22 11:27:04 +0800

    Gps

--- text2/main.c
+++ text2/main.c
@@ -69,11 +69,30 @@ int main(int argc, const char * argv[])
     
     
 }
-char time[10],gps,latitude1,longitude1,year[10],s[5];
-double latitude,longitude,v,course;
-FILE *fp2;
-fp2=fopen("//User//s20161104602//Desktop//test1//GPS)
+int main()
+{
+    FILE *fr;
+    char str1[70];
+    char str2[70];
+    char lat[9];
+    int i;
+    fr=fopen("//Users//s20161104602//Desktop//text2//GPD","r");
+    fscanf(fr,"%s%s",str1,str2");
+           printf("结果：%是\n%s\n",str1,str2);
+           for(i=0;i<8;i++)
+           lat[i]=str1[i+16];
+           lat[8]='\0';
+           printf("纬度:%s\n",lat);
+           fclose(fr);
+           return 0;
 
+
+
+
+
+
+
+}
 
 
 
